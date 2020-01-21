225f46bebba2366afcdf858a6826b74aa971a42c
bjmfransen <33018841+bjmfransen@users.noreply.github.com>
2020-01-21 16:59:23 +0100

    Update gen-html.js

--- gen-html.js
+++ gen-html.js
@@ -14,6 +14,11 @@ class HtmlElement {
       } else {
         //the argument is not a string so it must be a hash => load it
         json = arguments[0];
+        
+        //if the object does not contain an attribute key, add an empty one
+        if (!('attributes' in json)){
+          json.attributes = {};
+        }
       }
     } else {
       //case 2: there are three arguments: tag, attributes and content
@@ -21,8 +26,24 @@ class HtmlElement {
       json.attributes = arguments[1] || {};
       json.content = arguments[2] || '';
     }
-
+    console.log('loading', json)
+    
+    //any attribute in json that is not tag, attribute or content will be moved to attributes
+    for (let key in json){
+      console.log(key)
+      if (['tag', 'attributes', 'content'].indexOf(key) === -1){
+        console.log('inside', key);
+        json.attributes[key] = json[key];
+        delete json[key];
+      }
+    }
+    console.log('MODIFIED ', json)
     return this.load(json);
+  }
+  
+  setAttribute(key, value){
+    this.attributes[key] = value;
+    return this;
   }
   
   appendChild(){
@@ -36,14 +57,16 @@ class HtmlElement {
   }
 
   load(json){
-    //json: has three keys
+    //json: has four possible keys
     // tag - string
     // attributes - key/string value pairs
     // content - [json||string]
+    // class - string - will be moved to attributes as class
     
     //start by processing content
     //if it is not an array, make it so
     let content;
+ 
     if (json.content instanceof Array){
       content = json.content;
     } else {
@@ -125,14 +148,20 @@ let heJson = {
 // }))
 
 class HtmlDocument {
-  constructor(){
+  constructor(json){
     this.html = new HtmlTag('html');
-    this.head = new HtmlTag('head');
-    this.body = new HtmlTag('body');
-    
-    this.html.appendChild(this.head);
-    this.html.appendChild(this.body);
+    if (typeof json === 'undefined'){
+      this.head = new HtmlTag('head');
+      this.body = new HtmlTag('body');
+
+      this.html.appendChild(this.head);
+      this.html.appendChild(this.body);    
+    } else {
+      this.loadHtml(json)
+    }
   }
+  
+
   
   appendScript(content, place){
     if (typeof place === 'undefined'){
@@ -144,7 +173,17 @@ class HtmlDocument {
         'type': 'text/javascript'
       },
       content: content
-    })      
+    })
+  }
+  
+  appendStyle(content, place){
+    if (typeof place === 'undefined'){
+      place = this.head;
+    }
+    place.appendChild({
+      'tag': 'style',
+      content: content
+    })
   }
   
   toString(){
@@ -154,6 +193,72 @@ class HtmlDocument {
     return result;
   }
 }
+/*
 let hDoc = new HtmlDocument()
-hDoc.appendScript('console.log("lalala")')
+hDoc.html.setAttribute('lang', 'en');
+hDoc.head
+  .appendChild('meta', {
+    'charset': 'UTF-8'
+  })
+  .appendChild('link', {
+    rel: 'stylesheet',
+    href: 'css/overview.css'
+  })
+  .appendChild('script', {
+    src: 'https://kit.fontawesome.com/e5b76b1e14.js',
+    crossorigin: 'anonymous'
+  })
+  .appendChild('title', null, 'FMTH22')
+
+hDoc.appendScript('console.log("lalala")', hDoc.body)
+hDoc.appendStyle('.bold { font-style: bold; }')
 console.log(hDoc+'')
+*/
+
+let he2 = new HtmlElement({
+  tag: 'html',
+  lang: 'en',
+  content: [{
+    tag: 'div',
+    class: 'question'
+  }]
+});
+
+
+const questionOverview = (labelNr, difficultyString, qnl, qen, answer) => {
+  return {
+    tag: 'div',
+    class: 'question',
+    content: [{
+      tag: 'div',
+      class: 'question-header',
+      content: [{
+        tag: 'div',
+        class: 'nr',
+        content: labelNr
+      },{
+        tag: 'div',
+        class: 'difficulty',
+        content: difficultyString
+      }]
+    },{
+      tag: 'div',
+      class: 'question-text',
+      content: [{
+        tag: 'div',
+        'data-language': 'nl',
+        content: qnl
+      },{
+        tag: 'div',
+        'data-language': 'en',
+        content: qen        
+      }]
+    },{
+      tag: 'div',
+      class: 'answer',
+      content: answer
+    }]
+  }
+}
+
+console.log(new HtmlElement(questionOverview('Vraag 1', '<diff>', 'Wat is de hoofdstad van Witrusland?', 'What is the capital of Belarus?', 'Minsk'))+'')
